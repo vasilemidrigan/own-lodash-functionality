1f91468faa5c3545615e31a219ba2430976860fa
Vasile Midrigan <111rigan111@gmail.com>
2021-08-29 20:06:56 +0100

    Implemented _.words() and _.pad()

--- _.js
+++ _.js
@@ -1,4 +1,6 @@
 const _ = {
+    //    clamp(yourNumber, lowerBound, upperBound) {}
+    //clamps number within the inclusice lower and upper bounds.
     clamp(number, lowerBound, upperBound) {
         if(number < lowerBound) {
             return lowerBound;
@@ -8,6 +10,10 @@ const _ = {
             return upperBound;
         }
     },
+    //    inRange(yourNumber, startRange, endRange) {}
+    // Checks if n is between startValue and up to, but not including, endValue. 
+    // If endValue is not specified, it's set to start with startValue then set to 0. 
+    // If startValue is greater than endValue the params are swapped to support negative ranges.
     inRange(nr, startValue, endValue) {
         if(endValue === undefined) {
             endValue = startValue;
@@ -24,9 +30,40 @@ const _ = {
             return false;
         } else if (nr >= startValue && nr < endValue) {
             return true;
+        }       
+    },
+    //    words(stringArg) {}
+    // A slightly simplified version of original lodash words() string method.
+    // The method takes one argument - a string.
+    // Splits the strings into an array of words.   
+    words(strings) {
+        const toArr = strings.split(' ');
+        return toArr;
+    },
+    //    pad(string, lenght) {}
+    //  A slightly simplified version of the original .pad() lodash method
+    //  With only whitespaces 
+    pad(string, length) {
+      if(length <= string.length) {
+          return string;
+      }
+      if(typeof length === 'number' && length > string.length) {
+        const pads = length - string.length; // 8 - 4 = 4
+        const padsDiv2 = pads / 2;           // 4 / 2 = 2
+        if(pads % 2 === 0) {
+            string = string.padStart(string.length + padsDiv2 , ' ');
+            string = string.padEnd(length, ' ');
+        } else if(pads % 2 != 0) {
+            const padStart = Math.floor(padsDiv2);
+            const padEnd = Math.floor(padsDiv2 + 1);
+            string = string.padStart(string.length + padStart, ' ');
+            string = string.padEnd(string.length + padEnd, ' ');
         }
+        return string;
+      }
     }
 };
+
 
 // Do not write or modify code below this line.
 module.exports = _;
